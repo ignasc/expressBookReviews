e6a50b234bc6216f4aedf64a7e2e733f5bb8a0ef
Ignas <ignui.c@gmail.com>
2023-08-20 13:17:30 +0100

    code modified to use promises. Original code snippets commented out.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,23 +21,63 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  let allBooks = JSON.stringify(books, null, 4);
-  return res.send(allBooks);
+  //ORIGINAL CODE WITHOUT PROMISES
+  /*let allBooks = JSON.stringify(books, null, 4);
+  return res.send(allBooks);*/
+
+  //UPDATED CODE WITH PROMISES
+  let allBooksPromise = new Promise((resolve, reject) => {
+    let allBooks = JSON.stringify(books, null, 4);
+    if(allBooks){
+      resolve(allBooks);
+    }else{
+      reject("There was a problem retrieving the list of books.")
+    };
+  })
+
+  allBooksPromise
+  .then((success) => {
+    return res.send(success);
+  })
+  .catch((err) => {
+    return res.status(404).json({"message": err})
+  })
+  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  let selectedBook = books[req.params.isbn];
+  //ORIGINAL CODE WITHOUT PROMISES
+  /*let selectedBook = books[req.params.isbn];
   if(selectedBook){
     return res.send(JSON.stringify(selectedBook, null, 4));
   }else{
     return res.status(300).json({message: "ISBN number not found in the database"});
-  };
+  };*/
+
+  //UPDATED CODE WITH PROMISES
+  let bookDetails = new Promise((resolve, reject) => {
+    let selectedBook = books[req.params.isbn];
+    if(selectedBook){
+      resolve(selectedBook);
+    }else{
+      reject(req.params.isbn)
+    };
+  })
+
+  bookDetails
+  .then((success) => {
+    return res.send(success);
+  })
+  .catch((err) => {
+    return res.status(404).json({"message": "There was a problem retrieving the details of the book or there is no such book under the ISBN number " + err})
+  })
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  let listOfBookKeys = Object.keys(books);
+  //ORIGINAL CODE WITHOUT PROMISES
+  /*let listOfBookKeys = Object.keys(books);
   let selectedByAuthor = {};
     for(let index = 0; index < listOfBookKeys.length; index++){
       if(req.params.author == books[listOfBookKeys[index]]["author"]){
@@ -49,12 +89,38 @@ public_users.get('/author/:author',function (req, res) {
     return res.status(300).json({"message": "No books found by author " + req.params.author})
   }else{
     return res.send(JSON.stringify(selectedByAuthor));
-  };
+  };*/
+
+  //UPDATED CODE WITH PROMISES
+  let bookDetails = new Promise((resolve, reject) => {
+    let listOfBookKeys = Object.keys(books);
+    let selectedByAuthor = {};
+    for(let index = 0; index < listOfBookKeys.length; index++){
+      if(req.params.author == books[listOfBookKeys[index]]["author"]){
+        selectedByAuthor[listOfBookKeys[index]] = books[listOfBookKeys[index]];
+      };
+    };
+    
+    if(Object.keys(selectedByAuthor).length == 0){
+      reject(req.params.author);
+    }else{
+      resolve(JSON.stringify(selectedByAuthor));
+    };
+  })
+
+  bookDetails
+  .then((success) => {
+    return res.send(success);
+  })
+  .catch((err) => {
+    return res.status(404).json({"message": "No books found by author " + err})
+  })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  let listOfBookKeys = Object.keys(books);
+  //ORIGINAL CODE WITHOUT PROMISES
+  /*let listOfBookKeys = Object.keys(books);
   let selectedByTitle = {};
     for(let index = 0; index < listOfBookKeys.length; index++){
       if(req.params.title == books[listOfBookKeys[index]]["title"]){
@@ -66,7 +132,32 @@ public_users.get('/title/:title',function (req, res) {
     return res.status(300).json({"message": "No books found named " + req.params.title})
   }else{
     return res.send(JSON.stringify(selectedByTitle));
-  };
+  };*/
+
+  //UPDATED CODE WITH PROMISES
+  let listOfBooksByTitlePromise = new Promise((resolve, reject) => {
+    let listOfBookKeys = Object.keys(books);
+    let selectedByTitle = {};
+    for(let index = 0; index < listOfBookKeys.length; index++){
+      if(req.params.title == books[listOfBookKeys[index]]["title"]){
+        selectedByTitle[listOfBookKeys[index]] = books[listOfBookKeys[index]];
+      };
+    };
+    
+    if(Object.keys(selectedByTitle).length == 0){
+      reject(req.params.title);
+    }else{
+      resolve(JSON.stringify(selectedByTitle));
+    };
+})
+
+listOfBooksByTitlePromise
+  .then((success) => {
+    return res.send(success);
+  })
+  .catch((err) => {
+    return res.status(300).json({"message": "No books found named " + err})
+  })
 });
 
 //  Get book review
